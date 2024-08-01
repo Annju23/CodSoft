e51238e95c65a877fe8568d5b8457fbbedcd7524
Annju23 <153422583+Annju23@users.noreply.github.com>
2024-08-01 22:16:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    updateDisplay();
+}
+
+function setOperator(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculate();
+    }
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+    updateDisplay();
+}
+
+function calculate() {
+    if (currentInput === '' || previousInput === '' || operator === '') return;
+    let result;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+    switch (operator) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    display.innerText = result;
+    currentInput = result.toString();
+    previousInput = '';
+    operator = '';
+}
+
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    operator = '';
+    display.innerText = '';
+}
+
+function updateDisplay() {
+    display.innerText = `${previousInput} ${operator} ${currentInput}`;
+}
